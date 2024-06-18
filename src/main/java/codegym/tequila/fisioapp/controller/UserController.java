ceef5fa7d0336f1705160a878d42a041bd8efa85
Ivan Uresti <iuresti@skytouchtechnology.com>
2024-06-17 18:16:43 -0600

    QUité final por prueba

--- src/main/java/codegym/tequila/fisioapp/controller/UserController.java
+++ src/main/java/codegym/tequila/fisioapp/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RequestMapping("/api/user")
 public class UserController {
 
-    private final UserService userService;
+    private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
