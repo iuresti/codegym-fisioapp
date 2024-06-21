c91b364d9af72b60f51a17436f94181940061bf1
Ivan Uresti <iuresti@skytouchtechnology.com>
2024-06-20 19:23:06 -0600

    Añadir Logger

--- src/main/java/codegym/tequila/fisioapp/controller/FisioAppExceptionHandler.java
+++ src/main/java/codegym/tequila/fisioapp/controller/FisioAppExceptionHandler.java
@@ -1,5 +1,7 @@
 package codegym.tequila.fisioapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,8 +12,13 @@ import java.util.NoSuchElementException;
 @ControllerAdvice
 public class FisioAppExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(FisioAppExceptionHandler.class);
+
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Void> handleNoSuchElementException() {
+    public ResponseEntity<Void> handleNoSuchElementException(NoSuchElementException exception) {
+
+        logger.error("No such element exception", exception);
+
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
